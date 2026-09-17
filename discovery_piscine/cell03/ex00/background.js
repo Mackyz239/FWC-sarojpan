c073afd16f88c161ea42028b0bdcbd40a4012f4c
Mackyz23 <kunmax2548@gmail.com>
2026-09-18 04:25:24 +0700

    siuuu

---
+++ discovery_piscine/cell03/ex00/background.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("change-background");
+
+button.addEventListener("click", function () {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+});
